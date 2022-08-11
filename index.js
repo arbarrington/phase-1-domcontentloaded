bea3d045a0f0aa7451f8dac3542dc59e24ca4d76
Austin Barrington <arbarrington12@gmail.com>
2022-08-11 17:12:09 -0600

    commit tree 96d3db291bda15241390557c86fcf706d6c89ff0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function () {
+    const statement = document.getElementById("text");
+    statement.textContent = "This is really cool!";
+    console.log("The DOM has loaded.");
+}
+)
+
+console.log("Ya code ain't ran yet");
